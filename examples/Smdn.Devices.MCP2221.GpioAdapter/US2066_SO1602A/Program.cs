de44e7d76764ee1b6d190d0e37b8713cd75a042c
smdn <smdn@smdn.jp>
2021-06-19 21:27:25 +0900

    add example 'US2066_SO1602A'

---
+++ examples/Smdn.Devices.MCP2221.GpioAdapter/US2066_SO1602A/Program.cs
@@ -0,0 +1,24 @@
+﻿// SPDX-FileCopyrightText: 2021 smdn <smdn@smdn.jp>
+// SPDX-License-Identifier: MIT
+
+using System;
+using System.Threading.Tasks;
+using System.Reflection;
+
+using Smdn.Devices.MCP2221;
+using Smdn.Devices.MCP2221.GpioAdapter;
+using Smdn.Devices.US2066;
+
+await using var device = await MCP2221.OpenAsync();
+
+await device.GP3.ConfigureAsLEDI2CAsync();
+
+using var display = SO1602A.Create(
+  new MCP2221I2cDevice(device.I2C, SO1602A.DefaultI2CAddress) {
+    BusSpeed = I2CBusSpeed.FastMode
+  }
+);
+
+// write string and display it
+display.WriteLine("Hello, MCP2221A");
+display.WriteLine($"{device.GetType().Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion}");

426ae15551e82789c3a65892dcf941730cb854a1
smdn <smdn@smdn.jp>
2022-03-15 22:35:45 +0900

    add IsExternalInit

---
+++ src/Smdn.Devices.MCP2221/System.Runtime.CompilerServices/IsExternalInit.cs
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: 2021 smdn <smdn@smdn.jp>
+// SPDX-License-Identifier: MIT
+
+#if !SYSTEM_RUNTIME_COMPILERSERVICES_ISEXTERNALINIT
+namespace System.Runtime.CompilerServices;
+
+internal sealed class IsExternalInit { }
+#endif
